1de66e1b9ed4651af4689513e7426864560fc50d
unknown <gspatwal81@gmail.com>
2021-05-30 20:51:44 -0400

    adding to README.md file

--- code/production.py
+++ code/production.py
@@ -2,11 +2,11 @@
 import pandas as pd
 import zipfile as zf
 
-# # read the zip file as a pandas df
-# zip_file = zf.ZipFile('../resources/data/Production_Quality_Prediction/data_X.zip') 
-# df_X = pd.read_csv(zip_file.open('data_X.csv'))
-# # display df_X
-# print(df_X)
+# read the zip file as a pandas df
+zip_file = zf.ZipFile('../resources/data/Production_Quality_Prediction/data_X.zip') 
+df_X = pd.read_csv(zip_file.open('data_X.csv'))
+# display df_X
+print(df_X)
 
 # import csv file as a df
 df_Y = pd.read_csv('../resources/data/Production_Quality_Prediction/data_Y.csv')
